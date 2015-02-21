76c95cad03c7423d61cd47bf04b1f46f2b92d5e2
Goessler, Florian <florian.goessler@immobilienscout24.de>
2015-02-21 22:59:44 +0100

    added a search bar to the UserDefaultsInspectorVC - only iOS8+ cause it uses UISearchController

--- Pod/Classes/FGUserDefaultsInspectorViewController.m
+++ Pod/Classes/FGUserDefaultsInspectorViewController.m
@@ -4,9 +4,11 @@
 #import "FGUserDefaultsFormatter.h"
 
 
-@interface FGUserDefaultsInspectorViewController () <UIActionSheetDelegate>
+@interface FGUserDefaultsInspectorViewController () <UIActionSheetDelegate, UISearchResultsUpdating, UISearchControllerDelegate>
 @property(nonatomic, strong) NSDictionary *dictionaryRepresentation;
+@property(nonatomic, strong) NSDictionary *filteredDictionaryRepresentation;
 @property(nonatomic) BOOL showAllKeys;
+@property(nonatomic, strong) UISearchController *searchController;
 @end
 
 @implementation FGUserDefaultsInspectorViewController
@@ -17,6 +19,19 @@
     self.title = @"NSUserDefaults Inspector";
 
     [self.tableView registerClass:[FGUserDefaultsInspectorCell class] forCellReuseIdentifier:@"cell"];
+
+    if([UISearchController class]) {
+        self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+        self.searchController.searchResultsUpdater = self;
+        self.searchController.delegate = self;
+        self.searchController.dimsBackgroundDuringPresentation = NO;
+        self.searchController.searchBar.frame = CGRectMake(self.searchController.searchBar.frame.origin.x, self.searchController.searchBar.frame.origin.y, self.searchController.searchBar.frame.size.width, 44.0);
+
+        self.tableView.tableHeaderView = self.searchController.searchBar;
+
+        self.definesPresentationContext = YES;
+    }
+
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(_showActionItems:)];
 
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_updateList) name:NSUserDefaultsDidChangeNotification object:nil];
@@ -31,6 +46,7 @@
     } else {
         self.dictionaryRepresentation = [userDefaults persistentDomainForName:[NSBundle mainBundle].bundleIdentifier];
     }
+    self.filteredDictionaryRepresentation = self.dictionaryRepresentation;
 
     [self.tableView reloadData];
 }
@@ -44,14 +60,14 @@
 #pragma mark table view delegate & data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.dictionaryRepresentation.count;
+    return self.filteredDictionaryRepresentation.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
 
-    id key = self.dictionaryRepresentation.allKeys[(NSUInteger) indexPath.row];
-    id value = self.dictionaryRepresentation[key];
+    id key = self.filteredDictionaryRepresentation.allKeys[(NSUInteger) indexPath.row];
+    id value = self.filteredDictionaryRepresentation[key];
 
     cell.textLabel.text = [FGUserDefaultsFormatter descriptionForObject:value];
     cell.detailTextLabel.text = [@"Key: " stringByAppendingString:[FGUserDefaultsFormatter descriptionForObject:key]];
@@ -60,10 +76,31 @@
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    id key = self.dictionaryRepresentation.allKeys[(NSUInteger) indexPath.row];
-    id value = self.dictionaryRepresentation[key];
+    id key = self.filteredDictionaryRepresentation.allKeys[(NSUInteger) indexPath.row];
+    id value = self.filteredDictionaryRepresentation[key];
     FGUserDefaultsEditViewController *editVC = [[FGUserDefaultsEditViewController alloc] initWithKey:key value:value];
     [self.navigationController pushViewController:editVC animated:YES];
+}
+
+#pragma mark UISearchController stuff
+
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController {
+    NSString *searchString = [self.searchController.searchBar text];
+
+    NSMutableDictionary *filtered = [[NSMutableDictionary alloc] init];
+    for(id key in self.dictionaryRepresentation) {
+        if(([key respondsToSelector:@selector(containsString:)] && [key containsString:searchString]) || [searchString isEqualToString:@""]) {
+            filtered[key] = self.dictionaryRepresentation[key];
+        }
+    }
+    self.filteredDictionaryRepresentation = filtered;
+
+    [self.tableView reloadData];
+}
+
+- (void)didDismissSearchController:(UISearchController *)searchController {
+    self.filteredDictionaryRepresentation = self.dictionaryRepresentation;
+    [self.tableView reloadData];
 }
 
 #pragma mark UIActionSheetDelegate
